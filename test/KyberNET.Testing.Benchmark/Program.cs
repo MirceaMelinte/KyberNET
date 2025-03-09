dd5d3fbe0f203a6fbd29c60c01cc0ed0b4771bfc
mim <mim@MacBook-Pro.local>
2025-03-09 13:42:13 +0100

    Add test projects

---
+++ test/KyberNET.Testing.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace KyberNET.Testing.Benchmark
+{
+    using System.Threading.Tasks;
+
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ test/KyberNET.Testing.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace KyberNET.Testing.Unit
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
