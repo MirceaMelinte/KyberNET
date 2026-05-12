7414c66dc8dcbfe9444c9b3e064a92523bf07bd2
MirceaMelinte <31826664+MirceaMelinte@users.noreply.github.com>
2026-05-12 21:41:32 +0300

    Fix Q half rounding/decompress constant

--- src/KyberNET/Constants/KyberConstants.cs
+++ src/KyberNET/Constants/KyberConstants.cs
@@ -7,7 +7,7 @@ internal static class KyberConstants
     public const int N_BYTES = N >> 3;
     public const int Q = 3329;
     public const int Q_INV = -62209; // –q^(-1) mod 2^16
-    public const int Q_HALF = 1665;
+    public const int Q_HALF = Q >> 1;
     public const int BARRETT_APPROX = 20159;
     public const int MONT_R2 = 1353;
     public const int SECRET_KEY_LENGTH = N_BYTES;
